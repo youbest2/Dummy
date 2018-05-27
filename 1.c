cf3199cdbe8cd107afd0366c65da3f25027b01d9
Rajeev Ranjan <youbest2@hotmail.com>
2018-05-27 11:19:56 +0530

    Modification_1_amended

--- 1.c
+++ 1.c
@@ -1,10 +1,34 @@
-#include <studio.h>
-
-
+#include <stdio.h>
 int main()
 {
-
-  printf("Welcome to GitHub");
-  printf("By RAJEEV RANJAN");
+	printf("Welcome to GitHub\n");
+	printf("By RAJEEV RANJAN\n");
+	printf("Modification 2\n") ;
+	printf("Modification 2\n");
+	printf("Modification_2_1\n");
 
 }
+
+/*
+
+    npp_save
+
+    cd "$(CURRENT_DIRECTORY)"
+
+    g++ "$(FILE_NAME)" -o $(NAME_PART) -march=native -O3
+
+    NPP_RUN $(NAME_PART)
+
+	
+    # WORKING
+    NPP_SAVE
+
+    CD $(CURRENT_DIRECTORY)
+
+    C:\Program Files (x86)\CodeBlocks\MinGW\bin\gcc.exe -g "$(FILE_NAME)"
+
+    a
+
+
+
+*/
